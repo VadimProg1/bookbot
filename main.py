30198428d24add87ff63d294e6ba7072c6126a92
VadimProg <donzxz201@gmail.com>
2025-04-26 21:05:09 +0700

    added argument for tool

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ from stats import get_book_text
 from stats import get_num_words
 from stats import get_char_dictionary
 from stats import get_clean_sorted_char_list_by_count
-
-path_to_file = "./books/frankenstein.txt"
+import sys
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_file = sys.argv[1]
     num_words = get_num_words(get_book_text(path_to_file))
     dictionary = get_char_dictionary(get_book_text(path_to_file))
     sorted_char_list = get_clean_sorted_char_list_by_count(dictionary)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_file}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
     for i in sorted_char_list:
         print(f"{i}: {dictionary[i]}")
     print("============= END ===============")
-    
+
 main()
